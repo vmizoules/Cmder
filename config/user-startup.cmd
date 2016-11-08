e7dc506b36b0dfd5c4765169178ea69ca9592da1
Vince <>
2016-11-08 17:19:41 +0100

    Update to cmder v1.3.1

---
+++ config/user-startup.cmd
@@ -0,0 +1,6 @@
+:: use this file to run your own startup commands 
+:: use @ in front of the command to prevent printing the command
+ 
+:: @call "D:\Logiciels\CmderPortable\vendor\git-for-windows/cmd/start-ssh-agent.cmd
+:: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
+ 
